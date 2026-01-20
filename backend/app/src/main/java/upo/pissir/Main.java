83b304b8c9988f460a1fd2302397d7d3c6044ada
robert <roberto.zuzze@gmail.com>
2026-01-20 12:32:46 +0100

    fix: setup gradle

---
+++ backend/app/src/main/java/upo/pissir/Main.java
@@ -0,0 +1,12 @@
+package upo.pissir;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Pissir backend starting...");
+    }
+
+    public String getGreeting() {
+        return "Hello, World!";
+    }
+}
